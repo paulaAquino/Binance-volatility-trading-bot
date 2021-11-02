bf9ca9db9041e85c94cbe88904a4f2ed8d48575b
Paula <paula.aquino@condoconta.com.br>
2021-11-02 11:29:39 -0300

    add my configs

--- signalsample.py
+++ signalsample.py
@@ -11,11 +11,11 @@ MY_EXCHANGE = 'BINANCE'
 MY_SCREENER = 'CRYPTO'
 MY_FIRST_INTERVAL = Interval.INTERVAL_1_MINUTE
 MY_SECOND_INTERVAL = Interval.INTERVAL_5_MINUTES
-TA_BUY_THRESHOLD = 18 # How many of the 26 indicators to indicate a buy
+TA_BUY_THRESHOLD = 10  # How many of the 26 indicators to indicate a buy
 PAIR_WITH = 'USDT'
 TICKERS = 'signalsample.txt'
-TIME_TO_WAIT = 4 # Minutes to wait between analysis
-FULL_LOG = False # List anylysis result to console
+TIME_TO_WAIT = 2  # Minutes to wait between analysis
+FULL_LOG = True  # List anylysis result to console
 
 def analyze(pairs):
     taMax = 0
@@ -50,13 +50,13 @@ def analyze(pairs):
             first_analysis = first_handler[pair].get_analysis()
             second_analysis = second_handler[pair].get_analysis()
         except Exception as e:
-                    print("Exeption:")
-                    print(e)
-                    print (f'Coin: {pair}')
-                    print (f'First handler: {first_handler[pair]}')
-                    print (f'Second handler: {second_handler[pair]}')
-                    tacheckS = 0
-                
+            print("Exeption:")
+            print(e)
+            print (f'Coin: {pair}')
+            print (f'First handler: {first_handler[pair]}')
+            print (f'Second handler: {second_handler[pair]}')
+            tacheckS = 0
+
         first_tacheck = first_analysis.summary['BUY']
         second_tacheck = second_analysis.summary['BUY']
         if FULL_LOG:
@@ -65,18 +65,19 @@ def analyze(pairs):
             print(".", end = '')
 
         if first_tacheck > taMax:
-                taMax = first_tacheck
-                taMaxCoin = pair
+            taMax = first_tacheck
+            taMaxCoin = pair
         if first_tacheck >= TA_BUY_THRESHOLD and second_tacheck >= TA_BUY_THRESHOLD:
-                signal_coins[pair] = pair
-                print("")
-                print(f'Signal detected on {pair}')
-                with open('signals/signalsample.exs','a+') as f:
-                    f.write(pair + '\n')
+            signal_coins[pair] = pair
+            print("")
+            print(f'Signal detected on {pair}')
+            with open('signals/signalsample.exs','a+') as f:
+                f.write(pair + '\n')
     print("")
     print(f'Max signal by {taMaxCoin} at {taMax} on shortest timeframe') 
 
     return signal_coins
+
 
 if __name__ == '__main__':
     signal_coins = {}
@@ -92,6 +93,6 @@ if __name__ == '__main__':
         if len(signal_coins) == 0:
             print(f'No coins above {TA_BUY_THRESHOLD} threshold')
         else:
-            print(f'{len(signal_coins)} coins above {TA_BUY_THRESHOLD} treshold on both timeframes')
+            print(f'{len(signal_coins)} coins above {TA_BUY_THRESHOLD} threshold on both timeframes')
         print(f'Waiting {TIME_TO_WAIT} minutes for next analysis')
         time.sleep((TIME_TO_WAIT*60))
--- signalsamplemod.py
+++ signalsamplemod.py
@@ -12,11 +12,11 @@ MY_EXCHANGE = 'BINANCE'
 MY_SCREENER = 'CRYPTO'
 MY_FIRST_INTERVAL = Interval.INTERVAL_1_MINUTE
 MY_SECOND_INTERVAL = Interval.INTERVAL_5_MINUTES
-TA_BUY_THRESHOLD = 18 # How many of the 26 indicators to indicate a buy
+TA_BUY_THRESHOLD = 14  # How many of the 26 indicators to indicate a buy
 PAIR_WITH = 'USDT'
 TICKERS = 'signalsample.txt'
-TIME_TO_WAIT = 4 # Minutes to wait between analysis
-FULL_LOG = False # List anylysis result to console
+TIME_TO_WAIT = 4  # Minutes to wait between analysis
+FULL_LOG = False  # List anylysis result to console
 
 def analyze(pairs):
     taMax = 0
